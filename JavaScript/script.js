5b35001c0d95dd74e99eaebca434130806df7256
sthakeshar <shresthakeshar291@gmail.com>
2026-01-24 16:35:39 +0900

    some content added to display container

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,38 @@
-const myLibrary=[];
+const myLibrary=[
+                    {
+                        Name:'kns',
+                        author:'kns',
+                        pages:120,
+                        read:true
+                    },
+                    {
+                        Name:'kns1',
+                        author:'kns',
+                        pages:120,
+                        read:true
+                    },
+                    {
+                        Name:'kns2',
+                        author:'kns',
+                        pages:120,
+                        read:true
+                    }       
+                ];
 
+
+
+const displayBook = document.getElementById('display-book-container');
+
+let html = '';
+
+myLibrary.map(book => {
+    html+=`<div>
+                <ul>
+                    <li>${book.Name}</li>
+                    <li>${book.author}</li>
+                    <li>${book.pages}</li>
+                    <li>${book.read}</li>
+                </ul>
+            </div>`;
+}).join('');
+displayBook.innerHTML += html;
